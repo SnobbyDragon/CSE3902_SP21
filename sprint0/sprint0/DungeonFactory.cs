b20025cb670a4817f065e8ce7c44a765fc9cb7c4
Hajohnson3m <hajohnson3m@gmail.com>
2021-02-07 20:54:44 -0500

    Implemented DungeonFactory

--- sprint0/sprint0/DungeonFactory.cs
+++ sprint0/sprint0/DungeonFactory.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace sprint0
 {
     public class DungeonFactory
     {
-        public DungeonFactory()
+        private Game1 game;
+        private Texture2D texture;
+
+        public DungeonFactory(Game1 game)
+        {
+            this.game = game;
+            texture = game.Content.Load<Texture2D>("Images/DungeonTileset");
+
+        }
+
+        public ISprite MakeSprite(String spriteType, Vector2 location)
         {
 
+            switch (spriteType)
+            {
+
+                case "block":
+                    return new Block(texture, location);
+                default:
+                    throw new ArgumentException("Invalid sprite! Sprite factory failed.");
+            }
         }
     }
 }
