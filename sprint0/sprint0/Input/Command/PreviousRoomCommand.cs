71ad162ac0ecf85a2d00e8de0b8f49ec57066aa0
Stuti Shah <stuti@Stuti-Shah-MacBook-Pro.local>
2021-03-06 01:48:58 -0500

    added functionality (COMMENTED OUT RN) to switch between room. x = previous room, c = next room (COMMENTED OUT). Since all xml files aren't created yet, switching to rooms 1 and 3-8 will cause an exception

---
+++ sprint0/sprint0/Input/Command/PreviousRoomCommand.cs
@@ -0,0 +1,18 @@
+﻿//Author: Stuti Shah
+namespace sprint0
+{
+    internal class PreviousRoomCommand : ICommand
+    {
+        private Game1 game;
+        public PreviousRoomCommand(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            game.roomIndex = (game.roomIndex - 1 + 17) % 17;
+            game.changeRoom = true;
+        }
+    }
+}
--- sprint0/sprint0/Input/Controller/KeyboardController.cs
+++ sprint0/sprint0/Input/Controller/KeyboardController.cs
@@ -39,6 +39,13 @@ namespace sprint0
             RegisterCommand(Keys.D1, new OneCommand(game));
             RegisterCommand(Keys.D2, new TwoCommand(game));
             RegisterCommand(Keys.D3, new ThreeCommand(game));
+
+            /* NOTE:
+             * change room; commented out for now 
+             * since the corresponding code in the game class is commented out
+             */
+            //RegisterCommand(Keys.X, new PreviousRoomCommand(game));
+            //RegisterCommand(Keys.C, new NextRoomCommand(game));
         }
 
         public void RegisterCommand(Keys key, ICommand command)
@@ -49,7 +56,7 @@ namespace sprint0
         public void Update()
         {
             Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
-            
+
             foreach (Keys key in pressedKeys)
                 if (controllerMappings.ContainsKey(key) && (Array.IndexOf(previousPressedKeys, key) == -1) || movementKeys.Contains(key))
                     controllerMappings[key].Execute();
