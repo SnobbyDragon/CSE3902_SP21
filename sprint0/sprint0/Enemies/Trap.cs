fe4ba7d8ccb278c2ea267964b93bb48aaba17ce8
Hajohnson3m <hajohnson3m@gmail.com>
2021-03-10 11:31:09 -0500

    added movement for trap

--- sprint0/sprint0/Enemies/Trap.cs
+++ sprint0/sprint0/Enemies/Trap.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 //Author: Stuti Shah
+//Movemnet added by Hannah Johnson
 namespace sprint0
 {
     public class Trap : IEnemy
@@ -12,11 +13,14 @@ namespace sprint0
         public Texture2D Texture { get; set; }
         private Rectangle source;
         private readonly int width = 16, height = 16;
+        private Direction direction;
 
         public Trap(Texture2D texture, Vector2 location)
         {
             Location = new Rectangle((int)location.X, (int)location.Y, (int)(width * Game1.Scale), (int)(height * Game1.Scale));
             Texture = texture;
+
+            direction = Direction.n;
 
             //load sprite
             source = new Rectangle(164, 59, width, height);
@@ -29,12 +33,24 @@ namespace sprint0
 
         public void Update()
         {
-            // TODO SQUISH LINK
+            if (direction == Direction.s)
+            {
+                //moves sprite down
+                Location = new Rectangle(Location.X, Location.Y + 1, Location.Width, Location.Height);
+
+            }
+            else
+            {   //direction == Direction.n
+                //moves sprite up
+                Location = new Rectangle(Location.X, Location.Y - 1, Location.Width, Location.Height);
+
+            }
         }
 
-        public void ChangeDirection()
+            public void ChangeDirection()
         {
-            // TODO
-        }
+                Random random = new Random();
+                direction = (Direction)random.Next(0, 2);
+            }
     }
 }
