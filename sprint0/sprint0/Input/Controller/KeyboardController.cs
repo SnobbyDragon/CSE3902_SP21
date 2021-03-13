189d89911d10a9c45b63cb44c53fb020b6f8d3c9
Hajohnson3m <hajohnson3m@gmail.com>
2021-03-13 14:09:25 -0500

    * Room0.xml: added invisible block above ladder
    * Room1.xml: took away bow
    
    * KeyboardController.cs: deleted e command

--- sprint0/sprint0/Input/Controller/KeyboardController.cs
+++ sprint0/sprint0/Input/Controller/KeyboardController.cs
@@ -29,17 +29,11 @@ namespace sprint0
             RegisterCommand(Keys.Right, new RightCommand(game));
             RegisterCommand(Keys.Z, new SwordCommand(game));
             RegisterCommand(Keys.N, new SwordCommand(game));
-            RegisterCommand(Keys.E, new DamageCommand(game));
             RegisterCommand(Keys.D1, new OneCommand(game));
             RegisterCommand(Keys.D2, new TwoCommand(game));
             RegisterCommand(Keys.D3, new ThreeCommand(game));
 
-            /* NOTE:
-             * change room; commented out for now 
-             * since the corresponding code in the game class is commented out
-             */
-            //RegisterCommand(Keys.X, new PreviousRoomCommand(game));
-            //RegisterCommand(Keys.C, new NextRoomCommand(game));
+           
         }
 
         public void RegisterCommand(Keys key, ICommand command)
