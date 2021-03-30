087af3d359993659a36f9ba4915f4215bee29856
SnobbyDragon <25254660+SnobbyDragon@users.noreply.github.com>
2021-03-30 18:10:48 -0400

    fix color enum bug for spawning gels; fixed skip song bug

---
+++ sprint0/sprint0/ColorExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace sprint0
+{
+    public static class ColorExtension
+    {
+        private static readonly int knownColorMin = 28, knownColorMax = 167;
+
+        public static string GetName(this Color color)
+        {
+            for (int i = knownColorMin; i < knownColorMax; i++)
+            {
+                var knownColor = (System.Drawing.KnownColor)i;
+                var syscolor = System.Drawing.Color.FromKnownColor(knownColor);
+                if (syscolor.A == color.A && syscolor.R == color.R && syscolor.G == color.G && syscolor.B == color.B)
+                    return knownColor.ToString().ToLower();
+            }
+            throw new ArgumentException("Invalid color " + color + "! Cannot get name as string.");
+        }
+    }
+}
