75a498e85495131c68a782ef23bc54a01b2c285b
Stuti Shah <78219570+sshah-1@users.noreply.github.com>
2021-04-04 04:09:30 -0400

    item selection. small fixes to last pr

---
+++ sprint0/sprint0/Input/Command/LeftItemCommand.cs
@@ -0,0 +1,20 @@
+﻿// Author: Jesse He
+namespace sprint0
+{
+    class LeftCommand : ICommand
+    {
+        private readonly Game1 game;
+
+        public LeftCommand(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            if (game.stateMachine.GetState() != GameStateMachine.State.pause)
+                game.Room.Player.HandleLeft();
+            else game.universalScreenManager.pauseScreenManager.HandleLeft();
+        }
+    }
+}
