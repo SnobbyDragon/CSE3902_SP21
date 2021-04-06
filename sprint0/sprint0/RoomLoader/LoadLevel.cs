a878d3c2e31ddd4277d766e99db02b7bb8ad03be
NehaGupta <nehagpta4@gmail.com>
2021-04-05 20:59:40 -0400

    fixed link spawn location for when clicking thru

--- sprint0/sprint0/RoomLoader/LoadLevel.cs
+++ sprint0/sprint0/RoomLoader/LoadLevel.cs
@@ -22,6 +22,7 @@ namespace sprint0
         private readonly RoomEnemies roomEnemies;
         private readonly RoomSprite roomSprite;
         private readonly RoomEffect roomEffect;
+        public Dictionary<int,Vector2> locations;
 
         public LoadLevel(Game1 game, int roomIndex)
         {
@@ -35,7 +36,7 @@ namespace sprint0
             roomEffect = new RoomEffect(game);
         }
 
-        public void PopulateLists((List<ISprite>, List<IProjectile>, List<IBlock>, List<IEnemy>, List<INpc>, List<IItem>, List<IEffect>) roomElements)
+        public void PopulateLists((List<ISprite>, List<IProjectile>, List<IBlock>, List<IEnemy>, List<INpc>, List<IItem>, List<IEffect>, Dictionary<int, Vector2>) roomElements)
         {
             roomSprite.RoomSprites = roomElements.Item1;
             roomProjectile.Projectiles = roomElements.Item2;
@@ -44,6 +45,7 @@ namespace sprint0
             roomNPCs.NPCs = roomElements.Item5;
             roomItems.Items = roomElements.Item6;
             roomEffect.RoomEffects = roomElements.Item7;
+            locations = roomElements.Item8;
         }
 
         public void UpdateOffsets(Vector2 Offset) {
