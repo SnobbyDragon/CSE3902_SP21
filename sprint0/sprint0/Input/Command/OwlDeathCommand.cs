32ab70ecb3fc026defa0084074291708bdc558e8
Hajohnson3m <hajohnson3m@gmail.com>
2021-04-12 02:11:20 -0400

    Added Note sound classes and note commands, make owl die with certain sequence of nodes

---
+++ sprint0/sprint0/Input/Command/OwlDeathCommand.cs
@@ -0,0 +1,15 @@
+﻿// Author: Hannah Johnson
+namespace sprint0
+{
+    class OwlDeathCommand : ICommand
+    {
+        private readonly Game1 game;
+        public OwlDeathCommand(Game1 game) => this.game = game;
+        public void Execute()
+        {
+            int roomWithOwl = 19;
+            if(game.Room.RoomIndex==roomWithOwl)
+                game.Room.LoadLevel.RoomEnemies.KillOwl();
+        }
+    }
+}
