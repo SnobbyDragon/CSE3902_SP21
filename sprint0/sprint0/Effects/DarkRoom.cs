179e1b5e080ca923dad9c0afe1f97bb57aa48588
NehaGupta <nehagpta4@gmail.com>
2021-04-21 01:07:11 -0400

    room lights up when there is a fire projectile

--- sprint0/sprint0/Effects/DarkRoom.cs
+++ sprint0/sprint0/Effects/DarkRoom.cs
@@ -13,9 +13,9 @@ namespace sprint0
         public Rectangle Location { get; set; }
         public Texture2D Texture { get; set; }
         private Rectangle source;
-        private RenderTarget2D darkness, lightness;
+        private RenderTarget2D darkness;
         private GraphicsDevice graph;
-        private BlendState blend;
+        private Color brightness;
         int x, y;
         int width = 100;
         Game1 game;
@@ -25,7 +25,7 @@ namespace sprint0
             Texture = texture;
             this.game = game;
             source = new Rectangle(1023,60,20, 19);
-            
+            brightness = Color.Black;
             graph = game.GraphicsDevice;
             darkness = new RenderTarget2D(graph, Game1.Width + Game1.BorderThickness, Game1.MapHeight + Game1.BorderThickness);
             graph.SetRenderTarget(darkness);
@@ -37,12 +37,19 @@ namespace sprint0
         }
 
         public void Draw(SpriteBatch spriteBatch)
-        {
+        {                    
+            foreach (IProjectile project in game.Room.LoadLevel.RoomProjectile.Projectiles){
+                if (project is FlameProjectile)
+                {
+                    brightness = Color.Black;
+                    return;
+                }
+            }
             if (game.Room.Player.HasItem(PlayerItems.BlueCandle) || game.Room.Player.HasItem(PlayerItems.RedCandle))
             {               
                 x = (int)game.Room.Player.Pos.X-30;
                 y = (int)game.Room.Player.Pos.Y-30;
-                spriteBatch.Draw(Texture, new Rectangle(x, y, width, width),source, Color.White);
+                spriteBatch.Draw(Texture, new Rectangle(x, y, width, width),source, brightness);
             }
             else
             {
@@ -50,10 +57,10 @@ namespace sprint0
                 y = 0;
             }
 
-            spriteBatch.Draw(darkness, new Rectangle(0, Game1.HUDHeight * (int)Game1.Scale, x, (Game1.MapHeight+ Game1.BorderThickness*2)* (int)Game1.Scale), Color.White);
-            spriteBatch.Draw(darkness, new Rectangle(0, Game1.HUDHeight * (int)Game1.Scale, (Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale, y- Game1.HUDHeight * (int)Game1.Scale), Color.White);
-            spriteBatch.Draw(darkness, new Rectangle(0, y + width ,(Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale, ((Game1.MapHeight + Game1.HUDHeight + (Game1.BorderThickness*2))*(int)Game1.Scale)-y-width), Color.White);
-            spriteBatch.Draw(darkness, new Rectangle(x+width, Game1.HUDHeight * (int)Game1.Scale, (Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale, ((Game1.MapHeight + Game1.HUDHeight + (Game1.BorderThickness*2))*(int)Game1.Scale)), Color.White);           
+            spriteBatch.Draw(darkness, new Rectangle(0, Game1.HUDHeight * (int)Game1.Scale, x, (Game1.MapHeight+ Game1.BorderThickness*2)* (int)Game1.Scale), brightness);
+            spriteBatch.Draw(darkness, new Rectangle(x, Game1.HUDHeight * (int)Game1.Scale, (Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale -x , y- Game1.HUDHeight * (int)Game1.Scale), brightness);
+            spriteBatch.Draw(darkness, new Rectangle(x, y + width ,(Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale -x, ((Game1.MapHeight + Game1.HUDHeight + (Game1.BorderThickness*2))*(int)Game1.Scale)-y-width), brightness);
+            spriteBatch.Draw(darkness, new Rectangle(x+width, y, (Game1.Width + Game1.BorderThickness * 2) * (int)Game1.Scale, width), brightness);           
         }
 
         public void Update()
