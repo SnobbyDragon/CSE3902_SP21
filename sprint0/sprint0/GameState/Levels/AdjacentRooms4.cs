1ea4ce2c499becd062b06c52453b9039a8eadb4c
jacurick19 <34878168+jacurick19@users.noreply.github.com>
2021-04-22 17:47:31 -0400

    Added Adjacency list

--- sprint0/sprint0/GameState/Levels/AdjacentRooms4.cs
+++ sprint0/sprint0/GameState/Levels/AdjacentRooms4.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 
 namespace sprint0
 {
-    public static class AdjacentRooms1
+    public static class AdjacentRooms4
     {
         private static readonly Dictionary<int, Dictionary<Direction, int>> adjacentRooms = new Dictionary<int, Dictionary<Direction, int>>
             {
-                { 1, new Dictionary<Direction, int> { { Direction.East, 2 }, { Direction.West, 18 } } },
-                { 2, new Dictionary<Direction, int> { { Direction.West, 1 }, { Direction.South, 3 } } },
-                { 3, new Dictionary<Direction, int> { { Direction.North, 2 }, { Direction.South, 6 } } },
-                { 4, new Dictionary<Direction, int> { { Direction.East, 5 } } },
-                { 5, new Dictionary<Direction, int> { { Direction.West, 4 }, { Direction.East, 6 }, { Direction.South, 11 } } },
-                { 6, new Dictionary<Direction, int> { { Direction.North, 3 }, { Direction.South, 12 }, { Direction.East, 7 }, { Direction.West, 5 } } },
-                { 7, new Dictionary<Direction, int> { { Direction.East, 10 }, { Direction.South, 13 }, { Direction.West, 6 } } },
-                { 8, new Dictionary<Direction, int> { { Direction.East, 9 }, { Direction.South, 10 } } },
-                { 9, new Dictionary<Direction, int> { { Direction.West, 8 } } },
-                { 10, new Dictionary<Direction, int> { { Direction.North, 8 }, { Direction.West, 7 } } },
-                { 11, new Dictionary<Direction, int> { { Direction.East, 12 }, { Direction.North, 5 } } },
-                { 12, new Dictionary<Direction, int> { { Direction.East, 13 }, { Direction.North, 6 }, { Direction.West, 11 }, { Direction.South, 14 } } },
-                { 13, new Dictionary<Direction, int> { { Direction.West, 12 }, { Direction.North, 7 } } },
-                { 14, new Dictionary<Direction, int> { { Direction.North, 12 }, { Direction.South, 16 } } },
-                { 15, new Dictionary<Direction, int> { { Direction.East, 16 } } },
-                { 16, new Dictionary<Direction, int> { { Direction.West, 15 }, { Direction.East, 17 }, { Direction.North, 14 } } },
-                { 17, new Dictionary<Direction, int> { { Direction.West, 16 } } },
-                { 18, new Dictionary<Direction, int> { { Direction.East, 1 } } }
+                { 0, new Dictionary<Direction, int> { { Direction.South, 2 }, { Direction.East, 1 } } },
+                { 1, new Dictionary<Direction, int> { { Direction.West, 0 }, { Direction.South, 3 } } },
+                { 2, new Dictionary<Direction, int> { { Direction.North, 0 }, { Direction.East, 3 } } },
+                { 3, new Dictionary<Direction, int> { { Direction.East, 4 }, { Direction.West, 2 }, { Direction.North, 1 } } },
+                { 4, new Dictionary<Direction, int> { { Direction.West, 3}, { Direction.South, 9 }, { Direction.East, 5 } } },
+                { 5, new Dictionary<Direction, int> { { Direction.North, 8 }, { Direction.East, 4 }, { Direction.West, 6 } } },
+                { 6, new Dictionary<Direction, int> { { Direction.East, 5 }, { Direction.North, 7 } } },
+                { 7, new Dictionary<Direction, int> { { Direction.East, 8 }, { Direction.South, 6 } } },
+                { 8, new Dictionary<Direction, int> { { Direction.East, 7 }, { Direction.South, 5 } } },
+                { 9, new Dictionary<Direction, int> { { Direction.North, 4}, { Direction.South, 10 } } },
+                { 10, new Dictionary<Direction, int> { { Direction.North, 9 }, { Direction.West, 11 } } },
+                { 11, new Dictionary<Direction, int> { { Direction.East, 10 }, { Direction.West, 12 } } },
+                { 12, new Dictionary<Direction, int> { { Direction.East, 11 }, { Direction.West, 13 } } },
+                { 13, new Dictionary<Direction, int> { { Direction.East, 12 } } },
             };
 
         public static int GetAdjacentRoom(int currRoomIndex, Direction side)
