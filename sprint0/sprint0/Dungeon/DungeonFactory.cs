4a58a8f4cad6a64e5974fb1e8833993e1cb1349d
NehaGupta <nehagpta4@gmail.com>
2021-04-23 00:36:30 -0400

    redid dark room because merge conflicts were being annoying

--- sprint0/sprint0/Dungeon/DungeonFactory.cs
+++ sprint0/sprint0/Dungeon/DungeonFactory.cs
@@ -8,7 +8,7 @@ namespace sprint0
     {
         RoomFloor, RoomBorder, Darkness, DownWall, RightWall, LeftWall, UpWall, DownOpenDoor, RightOpenDoor, LeftOpenDoor, UpOpenDoor,
         DownLockedDoor, RightLockedDoor, LeftLockedDoor, UpLockedDoor, DownShutDoor, RightShutDoor, LeftShutDoor, UpShutDoor,
-        DownBombedOpening, RightBombedOpening, LeftBombedOpening, UpBombedOpening
+        DownBombedOpening, RightBombedOpening, LeftBombedOpening, UpBombedOpening, DarkRoom
     }
 
     public enum BlockEnum
@@ -54,6 +54,7 @@ namespace sprint0
                 DungeonEnum.RightBombedOpening => new BombedOpening(texture, location, Direction.West, game.Rooms[roomIndex]),
                 DungeonEnum.LeftBombedOpening => new BombedOpening(texture, location, Direction.East, game.Rooms[roomIndex]),
                 DungeonEnum.UpBombedOpening => new BombedOpening(texture, location, Direction.South, game.Rooms[roomIndex]),
+                DungeonEnum.DarkRoom => new DarkRoom(texture, location, game),
                 _ => throw new ArgumentException("Invalid sprite! " + spriteType.ToString() + " Sprite factory failed."),
             };
         }
